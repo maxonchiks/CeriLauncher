ce788e8592b5d3de2cdee30773b9231f0fc238b3
maxonchiks <62903443+maxonchiks@users.noreply.github.com>
2020-03-30 22:32:43 +0300

    Update russian.dart
    I have translated this. Hope, that update can be soon.

--- translations/russian.dart
+++ translations/russian.dart
@@ -8,493 +8,493 @@ String translate(text) {
     //
 
     case "getting app info":
-      return text;
+      return "получение инфы о приложении";
     case "loading page":
-      return text;
+      return "загрузка страницы;
     case "loading preferences":
-      return text;
+      return "загрузка предпочтений;
     case "loading settings":
-      return text;
+      return "загрузка настроек";
 
     //
     // ----- SETUP -----
     //
 
     case "Welcome to":
-      return text;
+      return Добро пожаловать в;
     case "Ceri Launcher":
       return text;
 
     case "Swipe left to start":
-      return text;
+      return "Свайпни влево чтобы начать";
 
     case "Select language":
-      return text;
+      return "Выбери язык" ;
 
     case "All languages (other than English) are translated by the community. Some translations may be incorrect or missing.":
-      return text;
+      return "Все языки (кроме Английского) переведены сообществом. Переводы могут быть ошибочны.";
 
     case "More languages coming soon":
-      return text;
+      return "Скоро будет больше языков";
 
     case "Choose your style":
-      return text;
+      return "Выбери свой стиль";
 
     case "Choose style":
-      return text;
+      return "Выбери стиль";
 
     case "All apps":
-      return text;
+      return "Все приложения";
 
     case "Only favorites":
-      return text;
+      return "Только избранное";
 
     case "Tray":
-      return text;
+      return "Трей";
 
     case "You can always change this later":
-      return text;
+      return "Ты всегда можешь изменить это позже";
 
     case "Please choose an option to continue":
-      return text;
+      return "Пожалуйста, выбери опцию чтобы продолжить";
 
     case "Choose your layout":
-      return text;
+      return "Выбрать свой макет";
 
     case "Choose layout":
-      return text;
+      return "Выбрать макет";
 
     case "Grid":
-      return text;
+      return "Сетка";
 
     case "Honeycomb":
-      return text;
+      return "Соты";
 
     case "Choose your Ceri widget":
-      return text;
+      return "Выбрать свой Ceri виджет";
 
     case "Choose Ceri widget":
-      return text;
+      return "Выбрать Ceri виджет";
 
     case "None":
-      return text;
+      return "Нет";
 
     case "Light analog":
-      return text;
+      return "Светлый аналог";
 
     case "Dark analog":
-      return text;
+      return "Тёмный аналог";
 
     case "Digital":
-      return text;
+      return "Цифровой";
 
     case "Assistant":
-      return text;
+      return "Ассистент";
 
     case "User widget":
-      return text;
+      return "Пользовательский виджет";
 
     case "Choose your package":
-      return text;
+      return "Выбери свай пак";
 
     case "I'm an 18-year-old UK maths student trying to save money to study Cyber Security at university.":
-      return text;
+      return "Я 18-летний студент-матиматик из UK, пытаюсь собрать денег на учебу Кибер безопасности в университете";
     case "If you want access to all features and you don't mind sparing £2, I would be incredibly grateful!":
-      return text;
+      return "Если вы хотите открыть все возможности и вам не жалко потратить £2, я буду невероятно благодарен!";
 
     case "Upgrade to pro":
-      return text;
+      return "Обновить до PRO";
     case "Support me and access all current and future features!":
-      return text;
+      return "Поддержите меня и откройте все теперешние и будущие возможности!";
 
     case "Restore purchase":
-      return text;
+      return "Восстановить покупки";
     case "Already supported the project?":
-      return text;
+      return "Уже поддержал проэкт?";
 
     case "Stay with free version":
-      return text;
+      return "Остаться на бесплатной версии";
     case "Core functionality will always be free":
-      return text;
+      return "Основная функциональность всегда будет бесплатной";
 
     case "One last thing":
-      return text;
+      return "И ещё одна вещь";
 
     case "I rely completely on user feedback to help make Ceri Launcher better.":
-      return text;
+      return "Реально ценю ваши отзывы, они помогут сделать Ceri Launcher лучше";
     case "Please consider joining":
-      return text;
+      return "Пожалуйста, рассмотрите возможность присоединения";
     case "the Ceri Launcher subreddit":
-      return text;
+      return " к совершенствованию Ceri Launcher";
     case "or sending me an email with your thoughts.":
-      return text;
+      return "или отправьте мне эмейл со своими идеями";
     case "I'd appreciate it a lot!":
-      return text;
+      return "Я сильно ценю это!";
     case "Hope you enjoy,":
-      return text;
+      return "Надеюсь вам понравится!";
 
     case "Tap anywhere":
-      return text;
+      return "Тапните где-нибудь;
     case "to see your new home":
-      return text;
+      return чтобы увидеть новый рабочий стол";
 
     //
     // ----- HOMESCREEN -----
     //
 
     case "Swipe down to search":
-      return text;
+      return "Свайпни вниз для поиска";
 
     case "Pull down to search":
-      return text;
+      return "Опусти вниз для поиска";
 
     case "Long-press an app for options":
-      return text;
+      return "Зажми приложение для открытия настроек";
 
     case "Close":
-      return text;
+      return "Закрыть";
 
     case "Opens your Ceri Launcher settings":
-      return text;
+      return "Открыть настройки Ceri Launcher";
 
     case "Ceri Launcher Settings":
-      return text;
+      return "Настройки Ceri Launcher";
 
     case "Add to favorites":
-      return text;
+      return "В избранное";
 
     case "Remove from favorites":
-      return text;
+      return "Убрать из избранного";
 
     case "Add back to homescreen":
-      return text;
+      return "Отображать на экране";
 
     case "Hide from homescreen":
-      return text;
+      return "Убрать с экрана";
 
     case "Application info":
-      return text;
+      return "Инфо о приложении";
 
     case "Uninstall":
-      return text;
+      return "Удалить";
 
     //
     // ----- SEARCH -----
     //
 
     case "Search the web for":
-      return text;
+      return "Искать в сети";
 
     case "Search engine:":
-      return text;
+      return "Искать машинно";
 
     //
     // ----- SETTINGS -----
     //
 
     case "Set as home":
-      return text;
+      return "Установить как основной";
     case "Open default apps menu":
-      return text;
+      return "Открыть дефолтное меню";
 
     case "Device settings":
-      return text;
+      return "Настройки девайса";
     case "Open the standard settings app":
-      return text;
+      return "Открыть дефолтные настройки";
 
     case "Style":
-      return text;
+      return "Стиль";
     case "Choose style (All apps, Only favorites, Tray:, toggle app dimming":
-      return text;
+      return "Выбрать стиль (Всё приложухи, Только избранное, Трей, приложения";
 
     case "Wallpapers":
-      return text;
+      return "Обои";
     case "Choose wallpaper, apply dim/vignette":
-      return text;
+      return "Выбрать обои, виньетка/димминг";
 
     case "Dark mode":
-      return text;
+      return "Тёмный режим";
     case "True dark mode for OLED devices\n(or #000000 lovers:":
-      return text;
+      return "Тру тёмный режим для OLED девайсов";
 
     case "Configure layout":
-      return text;
+      return "Настроить макет";
     case "Standard grid or honeycomb, 2, 3 or 4 columns, customise icon size and padding":
-      return text;
+      return "Стандартная сетка или сотовая ячейка, 2, 3 или 4 столбца, настройка размера значков и отступов";
 
     case "Icon style":
-      return text;
+      return "Стиль иконок";
     case "Icon packs, shape, zoom/crop, shadows":
-      return text;
+      return "Наборы иконок, размеры, тени";
 
     case "Search options":
-      return text;
+      return "Поиск настроек";
     case "Search icon size, search engine":
-      return text;
+      return "Поиск размера иконок, поиск движка";
 
     case "Configure Ceri widget":
-      return text;
+      return "Конфигурация Ceri виджет";
     case "Choose Ceri widget, configure padding":
-      return text;
+      return "Выбрать Ceri виджет, настроить заполнение";
 
     case "Scrolling animations":
-      return text;
+      return "Анимация листания";
     case "None, bubble, straight, inwards, outwards, and more":
-      return text;
+      return "Нет, пузырь, ровно и тд.";
 
     case "Notification dots":
-      return text;
+      return "Точки уведомлений";
     case "See indications on your homescreen for pending notifications":
-      return text;
+      return "Индикация уведомлений";
 
     case "Gestures":
-      return text;
+      return "Жесты";
     case "Launch apps and actions quickly with side gestures":
-      return text;
+      return "Запускайте приложения и действия быстро с боковыми жестами";
 
     case "Miscellaneous settings":
-      return text;
+      return "Смешанные опции";
     case "Warning: Some of these may not have been fully tested":
-      return text;
+      return "Внимание: некоторые опции могут быть не полностью протестированы";
 
     case "Performance options":
-      return text;
+      return "Настройки производительности";
     case "Experimental settings that may improve performance":
-      return text;
+      return "Экспериментальные настройки, которые могут повысить производительность";
 
     case "About Ceri Launcher":
-      return text;
+      return "О приложении";
     case "Version number, developer info":
-      return text;
+      return "Номер версии, информация о разработчике";
 
     case "Check for updates":
-      return text;
+      return "Проверить обновления";
     case "Check for Ceri Launcher updates on the Play Store":
-      return text;
+      return "Проверить обновления на Play маркет";
 
     case "Support and feedback":
-      return text;
+      return "Поддержка и отзывы";
     case "Go to the Ceri Launcher subreddit":
-      return text;
+      return "Перейти к разработке";
 
     case "Email developer":
-      return text;
+      return "Почто разработчика";
     case "Have an issue or suggestion? Really enjoying Ceri Launcher? I try to reply within a few hours ::":
-      return text;
+      return "Проблемы? Предложения? Реально нравится Ceri Launcher? Я постараюсь ответить в течении пары часов::";
 
     case "Change language":
-      return text;
+      return "Изменить язык";
     case "Change the language for the Ceri Launcher interface":
-      return text;
+      return "Изменить язык интерфейса Ceri Launcher";
 
     case "Add or improve translations":
-      return text;
+      return "Добавить или улучшить перевод";
     case "Use your language skills to help make Ceri Launcher better. These translations will be added periodically in future updates.":
-      return text;
+      return "Используй свои языковые навыки, чтобы помочь сделать Ceri Launcher лучше. Эти переводы будут добавлены в будущих обновлениях.";
 
     case "Open source licenses":
-      return text;
+      return "Открыть источник лицензий";
 
     case "Version":
-      return text; // context: "Version 1.7.5-beta" etc.
+      return "Версия"; // context: "Версия 1.7.5-beta" etc.
     case "Paid — thanks!":
-      return text;
+      return "Оплачено - спасибо!";
     case "Free version":
-      return text;
+      return "Бесплатная версия";
     case "Your own":
-      return text;
+      return "Ваша собственность";
 
     case "Ceri Wallpapers":
-      return text;
+      return "Ceri обои";
     case "Android Home Xperience":
-      return text;
+      return "Android Home Xperience";
     case "Tiles below 'Your own' will open the respective wallpaper album in your browser.":
-      return text;
+      return "Откроет откроет соответствующий альбом обоев в вашем браузере" ;
     case "Vignette/dim":
-      return text;
+      return "Виньетка/димминг";
 
     case "Dark mode":
-      return text;
+      return "Тёмный режим";
     case "Standard":
-      return text;
+      return "Стандарт";
     case "Black (OLED:":
-      return text;
+      return "Чёрный (OLED:";
     case "Automatic":
-      return text;
+      return "Автоматически";
     case "Dark mode off":
-      return text;
+      return "Выкл тёмный режим";
     case "Dark mode on":
-      return text;
+      return "Вкл тёмный режим";
     case "Dark mode enables at night":
-      return text;
+      return "Включать тёмный режим ночью";
 
     case "Miscellaneous":
-      return text;
+      return "Смешанные";
     case "Disable haptics":
-      return text;
+      return "отключить гаптику";
     case "Show package names in search":
-      return text;
+      return "Показать имена паков в поиске";
     case "Disable vignette":
-      return text;
+      return "Отключить виньетку";
     case "Disable scrolling beyond screen height":
-      return text;
+      return "Отключить листание за экраном";
 
     case "2 columns":
-      return text;
+      return "2 столбца";
     case "3 columns":
-      return text;
+      return "3 столбца";
     case "Icon size":
-      return text;
+      return "Размер иконок";
     case "Icon padding":
-      return text;
+      return "Значки";
     case "Standard grid layout":
-      return text;
+      return "Стандартная сетка";
     case "Honeycomb layout":
-      return text;
+      return "Макет сот";
     case "Favorites stay at the top":
-      return text;
+      return "Избранное в топе";
     case "Just your favorites, nothing else":
-      return text;
+      return "Только избранное";
     case "Automatically show your most used apps only":
-      return text;
+      return "Показывать только часто используемые";
     case "(experimental:":
-      return text;
+      return "(экспериментально:";
     case "Dim apps that aren't in favorites":
-      return text;
+      return "Димить приложения, что в избранных";
 
     case "Nothing set":
-      return text;
+      return "Ничего не установлено";
     case "Select package":
-      return text;
+      return "Выбрать пак";
     case "Actions will be launchable with gestures in an upcoming update.":
-      return text;
+      return "Действия будут запускаться с помощью жестов в следующем обновлении";
     case "Search":
-      return text;
+      return "Поиск";
 
     case "No icon packs found.":
-      return text;
+      return "Паки иконок не найдены";
     case "None (default:":
-      return text;
+      return "Нет (стандарт:";
     case "Select icon pack":
-      return text;
+      return "Выбрать пак иконок";
     case "If you have an icon pack installed, choose it here":
-      return text;
+      return "Если у вас установлен пакет значков, выберите его здесь";
 
     case "Corner radius":
-      return text;
+      return "Радиус угла";
     case "Zoom/crop":
-      return text;
+      return "Зум/кроп";
     case "Shadows (off:":
-      return text;
+      return "Тени (выкл:";
     case "Shadows (subtle:":
-      return text;
+      return "Тени (тонкие:";
     case "Shadows (heavy:":
-      return text;
+      return "Тени (жирные:";
     case "Shadows (heavier:":
-      return text;
+      return "Тени (жесть:";
 
     case "App icon size":
-      return text;
+      return "Размер иконок";
 
     case "Animation strength":
-      return text;
+      return "Сила анимации";
     case "disabled":
-      return text;
+      return "отключено";
     case "weak":
-      return text;
+      return "слабо";
     case "normal":
-      return text;
+      return "нормально";
     case "strong":
-      return text;
+      return "сильно";
     case "strongest":
-      return text;
+      return "сильнейше";
 
     case "These are EXPERIMENTAL settings to try out if you suffer from poor performance or extreme battery drain when using Ceri Launcher.":
-      return text;
+      return "Это ЭКСПЕРИМЕНТАЛЬНЫЕ настройки, которые можно попробовать, если вы страдаете от плохой работы или чрезмерного разряда батареи при использовании Ceri Launcher.";
     case "If you are not having issues with performance it is recommended that you leave these at their defaults.":
-      return text;
+      return "Если у вас нет проблем с производительностью, рекомендуется оставить их по умолчанию.";
     case "Icon render size":
-      return text;
+      return "Размер рендера значка";
     case "Default is 192. Smaller values should result in lower RAM usage (at the expense of image quality:.":
-      return text;
+      return "По умолчанию 192. Меньшие значения должны привести к снижению использования оперативной памяти (за счет качества изображения :.";
     case "EXAMPLE":
-      return text;
+      return "Пример";
     case "Remove icons out of frame":
-      return text;
+      return "Убрать значки из рамки";
     case "Disable antialiasing":
-      return text;
+      return "Отключить антиалайзинг";
     case "Force icon caching":
-      return text;
+      return "Усилить кеширование значков";
 
     case "Ceri Launcher utilizes the works of these people:":
-      return text;
+      return "Ceri Launcher использует работы этих людей:";
 
     case "EXPERIMENTAL.":
-      return text;
+      return "ЭКСПЕРЕМЕНТАЛНО.";
 
     case "Nothing":
-      return text;
+      return "Ничего";
     case "Light analog clock widget":
-      return text;
+      return "Светлые аналоговые часы";
     case "by NPKompleet, The CS Guy and Ali Sayed":
-      return text;
+      return "by NPKompleet, The CS Guy and Ali Sayed";
     case "Dark analog clock widget":
-      return text;
+      return "Тёмные аналоговые часы";
     case "Digital clock":
-      return text;
+      return "Цифровые часы";
 
     //
     // ----- WIDGETS -----
     //
 
     case "Monday":
-      return text;
+      return "Понедельник";
     case "Tuesday":
-      return text;
+      return "Вторник";
     case "Wednesday":
-      return text;
+      return "Среда";
     case "Thursday":
-      return text;
+      return "Четверг";
     case "Friday":
-      return text;
+      return "Пятница";
     case "Saturday":
-      return text;
+      return "Суббота";
     case "Sunday":
-      return text;
+      return "Воскресенье";
 
     case "January":
-      return text;
+      return "Январь";
     case "February":
-      return text;
+      return "Февраль";
     case "March":
-      return text;
+      return "Март";
     case "April":
-      return text;
+      return "Апрель";
     case "May":
-      return text;
+      return "Май";
     case "June":
-      return text;
+      return "Июнь;
     case "July":
-      return text;
+      return "Июль";
     case "August":
-      return text;
+      return "Август";
     case "September":
-      return text;
+      return "Сентябрь";
     case "October":
-      return text;
+      return "Октябрь";
     case "November":
-      return text;
+      return "Ноябрь";
     case "December":
-      return text;
+      return "Декабрь";
 
     case "Good morning":
-      return text;
+      return "Доброе утро";
     case "Good afternoon":
-      return text;
+      return "Добрый день";
     case "Good evening":
-      return text;
+      return "Добрый вечер";
     case "Sleep well":
-      return text;
+      return "Спи сладко";
   }
 
   return text; // return original text if no translation found
